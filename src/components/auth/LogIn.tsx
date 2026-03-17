d1f90b1aadba6e267f58cea55ea44b758c4ed06f
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2026-03-17 12:16:05 +0200

    #REP-7: refactor for login and register

--- src/components/auth/LogIn.tsx
+++ src/components/auth/LogIn.tsx
@@ -1,3 +1,5 @@
+import { Input } from "../common";
+
 interface ILogInProps{
     goToDashboard: () => void;
 }
@@ -7,26 +9,8 @@ export const LogIn = ({goToDashboard}:ILogInProps) =>{
     return(
         <div className="justify-start flex flex-col w-10/12 h-auto">
           <form className=" flex flex-col gap-3">
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Email</span>
-              <input
-                required
-                type="email"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="example@yahoo.com"
-                autoComplete="false"
-              />
-            </label>
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Password</span>
-              <input
-                required
-                type="password"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="••••••••"
-                autoComplete="false"
-              />
-            </label>
+            <Input type="email" placeholder="Email" label="Email"/>
+            <Input type="password" placeholder="Password" label="Password"/>
             <button className="bg-core-slate hover:bg-power-purple transition duration-200 w-auto h-auto p-2 rounded-3xl hover:cursor-pointer"
             onClick={goToDashboard}>
                 LogIn
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,3 +1,5 @@
+import { Input } from "../common";
+
 interface IRegisterProps{
     goToDashboard: () => void;
 }
@@ -7,61 +9,23 @@ export const Register = ({goToDashboard}:IRegisterProps) =>{
         <div className="justify-start flex flex-col w-10/12 h-auto">
           <form className=" flex flex-col gap-3">
             <div className="flex gap-2">
-             <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">First name</span>
-              <input
-                required
-                type="text"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="Name"
-              />
-            </label>
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Last name</span>
-              <input
-                required
-                type="text"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="Name"
-              />
-            </label>
+             <Input type="text" placeholder="Name" label="First Name"/>
+             <Input type="text" placeholder="Name" label="Last Name"/>
             </div>
             <div className="flex gap-2">
-                <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Username</span>
-                <input
-                    required
-                    type="email"
-                    className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                    placeholder="username"
-                />
-                </label>
-                <label className="block text-sm">
-                <span className="mb-2 block text-slate-400">Email</span>
-                <input
-                    required
-                    type="email"
-                    className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                    placeholder="example@yahoo.com"
-                />
-                </label>
+             <Input type="text" placeholder="Username" label="Username"/>
+             <Input type="email" placeholder="example@yahoo.com" label="Email"/>
             </div>
             <div className="flex gap-2">
                 <div className="block text-sm w-full">
                     <span className="mb-2 block text-slate-400">Gender</span>
                         <div className="flex gap-2">
                             <label className="flex-1">
-                                <input
-                                type="radio"
-                                name="gender"
-                                value="male"
-                                className="peer hidden"
-                                required
-                                />
-                                <div className="bg-slate-700 border border-white/30 h-10 rounded-2xl flex items-center justify-center text-md cursor-pointer peer-checked:bg-purple-500 peer-checked:text-white">
-                                Male
-                                </div>
-                                </label>
+                                <input type="radio" name="gender" value="male" className="peer hidden"required/>
+                                    <div className="bg-slate-700 border border-white/30 h-10 rounded-2xl flex items-center justify-center text-md cursor-pointer peer-checked:bg-purple-500 peer-checked:text-white">
+                                        Male
+                                    </div>
+                            </label>
                             <label className="flex-1">
                                 <input type="radio" name="gender" value="female" className="peer hidden" />
                                     <div className="bg-slate-700 border border-white/30 h-10 rounded-2xl flex items-center justify-center text-md cursor-pointer peer-checked:bg-purple-500 peer-checked:text-white">
@@ -78,53 +42,14 @@ export const Register = ({goToDashboard}:IRegisterProps) =>{
                 </div>
             </div>
             <div className="flex gap-2">
-             <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Height</span>
-              <input
-                required
-                type="number"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="170"
-              />
-            </label>
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Width</span>
-              <input
-                required
-                type="number"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="80"
-              />
-            </label>
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Date of birth</span>
-              <input
-                required
-                type="date"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-              />
-            </label>
+             <Input type="number" placeholder="170" label="Height"/>
+             <Input type="number" placeholder="80" label="Weight"/>
+             <Input type="date" placeholder="" label="Date of birth"/>
             </div>
-             <div className="flex gap-2">
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Password</span>
-              <input
-                required
-                type="password"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="••••••••"
-              />
-            </label>
-            <label className="block text-sm">
-              <span className="mb-2 block text-slate-400">Confirm password</span>
-              <input
-                required
-                type="password"
-                className="bg-slate-700 border border-white/30 w-full h-10 rounded-2xl px-4 text-md"
-                placeholder="••••••••"
-              />
-            </label>
-              </div>
+            <div className="flex gap-2">
+             <Input type="password" placeholder="••••••••" label="Password"/>
+             <Input type="password" placeholder="••••••••" label="Confirm password"/>
+            </div>
             <button className="bg-core-slate hover:bg-power-purple transition duration-200 w-auto h-auto p-2 rounded-3xl hover:cursor-pointer"
             onClick={goToDashboard}>
                 Register
